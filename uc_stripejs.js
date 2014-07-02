4a3bff5c636b9f4c6437e5d2184a578c6d6c8e20
Harry Spink <harry@openpipe.co.uk>
2014-07-02 12:04:35 +0100

    Monitor the card status independantly
    and only stop the form from posting when we are certain the card is not valid. This value is stored in a global variable called ucStripeAllow.

--- uc_stripejs.js
+++ uc_stripejs.js
@@ -1,8 +1,9 @@
 jQuery(document).ready(function(){
-
     jQuery(".stripe-error-message").hide();
 
     Stripe.setPublishableKey(Drupal.settings.stripe_js_pk);
+    
+    var ucStripeAllow = false;
     var form = jQuery('.uc-cart-checkout-form');
 
     stripeResponseHandler = function(status, response) {
@@ -16,18 +17,16 @@ jQuery(document).ready(function(){
             jQuery("input[name=\"panes[payment][details][stripeToken]\"]").val(token);
             form.find("#edit-continue").attr("disabled", false);
 
+            ucStripeAllow = true;
             jQuery('#edit-continue').click();
         }
     };
 
     form.submit(function(event) {
-        event.preventDefault();
-
-        if(form.find('input[name="panes[payment][payment_method]"][value="stripe_js"]:checked').length !== 0){
+        if(!ucStripeAllow && parseInt(form.find('input[name="panes[payment][payment_method]"][value="stripe_js"]:checked').length) === 1){
+            event.preventDefault();
             form.find("#edit-continue").attr("disabled", true);
             Stripe.card.createToken(form, stripeResponseHandler);
-        } else {
-            jQuery('#edit-continue').click();
         }
     });
 });
